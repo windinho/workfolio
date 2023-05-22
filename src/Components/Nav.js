a125e7036cd41cbc125ba1e26fc00a4b2bd42e8f
windinho <arvnd7@gmail.com>
2023-05-22 16:30:07 +0000

    Update 3 files

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,16 +3,27 @@ import React from "react";
 // import IsMobile from "../CoreComponents/IsMobile";
 // import { Links } from "./Footer";
 
-const Nav= ({ isNavOpen }) => (
-		<div className={`nav-menu ${!isNavOpen ? "close" : ""}`}>
-			<ul>
-				<li className="title">Menu:</li>
-				{/* {Links("nav")} */}
-				<li className="social">
-					{/* {!IsMobile() && <Icon name="social" />} */}
-				</li>
-			</ul>
-		</div>
+// export const Links = (where) => {
+// 	return [
+// 		{ text: "About", href: "/about-us" },
+// 		{ text: "Work", href: "/work" },
+// 		{ text: "Hire Us", href: "/hire-us" },
+// 	].map(({ text, href }, i) => {
+// 		let a = <a href={href}>{text}</a>;
+// 		return <li key={i}>{where === "nav" ? <h4>{a}</h4> : <>{a}</>}</li>;
+// 	});
+// };
+
+const Nav = ({ isNavOpen }) => (
+  <div className={`nav-menu ${!isNavOpen ? "close" : ""}`}>
+    <ul>
+      <li className="title">Menu:</li>
+      {/* {Links("nav")} */}
+      <li className="social">
+        {/* {!IsMobile() && <Icon name="social" />} */}
+      </li>
+    </ul>
+  </div>
 );
 
-export default Nav
+export default Nav;
